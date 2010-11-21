027eba29b085f6a046f1cebc4f0fdd0348dca66f
Whyme.Lyu <5longluna@gmail.com>
2010-11-21 14:22:01 +0800

    Workspace watches servers with watchers

--- src/workspace.js
+++ src/workspace.js
@@ -5,16 +5,36 @@ var lml = require("./lml")
 
 function Workspace(dir) {
   this._workDir = dir
+  this._watchers = []
+  this._pages = {}
+  this._servers = []
 }
 lml.inherits(Workspace, lml.EventEmitter)
 
 lml.def(Workspace.prototype, {
   open: function(relPath) {
-    var page = new FileRoll
+    var page = new FileRoll()
       , absPath = path.join(this._workDir, relPath)
       , file = File.new(absPath)
     page.add(file)
+    this._pages[file.path] = page
     return page
+  }
+, useWatcher: function(watcher) {
+    watcher.on("relate", function(e) {
+      var parent = this._pages[e.belongTo]
+      if (parent) parent.add(File.new(e.path))
+    }.bind(this))
+    this._servers.forEach(function(server) {
+      server.on("request", watcher.closure)
+    })
+    this._watchers.push(watcher)
+  }
+, addServer: function(server) {
+    this._watchers.forEach(function(watcher) {
+      server.on("request", watcher.closure)
+    })
+    this._servers.push(server)
   }
 })
 
--- test/workspace.js
+++ test/workspace.js
@@ -5,10 +5,16 @@ var reut = require("reut")
   , File = roil.File
   , path = require("path")
   , touch = require("./fixture/touch")
+  , EventEmitter = require("events").EventEmitter
 
 reut.suite("Workspace Class")
 .setup(function(f, done) {
   f.ws = new Workspace(__dirname)
+  done()
+})
+.setup(function(f, done) {
+  f.fakeWatcher = new EventEmitter()
+  f.fakeWatcher.closure = function() {}
   done()
 })
 .test(".open()", function(t, f) {
@@ -20,4 +26,41 @@ reut.suite("Workspace Class")
     t.end()
   })
   touch(__filename)
+})
+.test(".addServer()", function(t, f) {
+  var ws = f.ws
+  ws.useWatcher(f.fakeWatcher)
+  ws.addServer({
+    on: t.cb(function(type, fn) {
+      t.equal(type, "request")
+      t.equal(fn, f.fakeWatcher.closure)
+    })
+  })
+})
+.setup(function(f, done) {
+  f.parent = File.new(path.join(__dirname, "/foo"))
+  f.child = File.new(path.join(__dirname, "/bar"))
+  done()
+})
+.teardown(function(f, done) {
+  f.parent.watchStop()
+  f.child.watchStop()
+  done()
+})
+.test(".useWatcher()", function(t, f) {
+  var ws = f.ws
+    , watcher = f.fakeWatcher
+    , page = ws.open("/foo")
+    , parent = f.parent
+    , child = f.child
+
+  ws.useWatcher(watcher)
+  page.add = t.cb(function(file) {
+    t.equal(file, child)
+  })
+
+  watcher.emit("relate", {
+    path: child.path
+  , belongTo: parent.path
+  })
 })
