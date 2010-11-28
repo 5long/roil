01b900f9407746d5413d57fd79731c7a0ccd3370
Whyme.Lyu <5longluna@gmail.com>
2010-11-29 00:59:31 +0800

    Rewrite frontend, considering YUI3

--- src/console/roil.js
+++ src/console/roil.js
@@ -3,22 +3,58 @@ var s = new io.Socket()
   , latest = null
 
 s.connect()
-s.on("message", function(msg) {
-  var e
-  try {msg = JSON.parse(msg)}
-  catch (e) {return}
-  if (msg && msg.type == "change") refresh(msg.url)
-})
 
-function start(url) {
-  s.send(JSON.stringify({
-    action: "open"
-  , url: url
-  }))
-  latest = pages[url] = window.open(url)
+function Page(url) {
+  var self = this
+  this.url = url
+  pages[url] = latest = this
+  this.backend.on("message", function(msg) {
+    msg = JSON.parse(msg)
+    if (msg && msg.type == "change") self.refresh()
+  })
 }
 
-function refresh(url) {
-  var page = pages[url]
-  page.location.reload()
+Page.prototype = {
+  backend: s
+, open: function() {
+    this.window = window.open(this.url)
+    this.backend.send(JSON.stringify({
+      action: "open"
+    , url: this.url
+    }))
+  }
+, refresh: function() {
+    this.window.location.reload()
+  }
+, close: function() {
+    this.window.close()
+  }
+, focus: function() {
+    this.window.focus()
+  }
+, toElement: function() {
+    var frag = document.createDocumentFragment()
+      , text = document.createTextNode()
+    text.nodeValue = this.url
+    frag.appendChild(text)
+    return frag
+  }
+}
+
+window.onload = function roilInit() {
+  var btn = document.getElementById("submitUrl")
+    , urlInput = document.getElementById("url")
+    , pageList = document.getElementById("pages")
+  if (!btn || !urlInput) return
+  btn.onclick = function() {
+    var url = urlInput.value.replace(/^\s+|\s+$/)
+      , page, pageElem
+    if (url[0] !== "/") url = "/" + url
+    if (url in pages) return
+    page = pages[url] = new Page(url)
+    page.open()
+    pageElem = document.createElement("li")
+    pageElem.appendChild(page.toElement())
+    pageList.appendChild(pageElem)
+  }
 }
